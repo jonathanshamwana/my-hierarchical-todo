391a53c5914a068d81a2fade43a7c24093d5a1f9
sp-jonathan <jonathanshamwana.visa@gmail.com>
2024-10-07 22:39:15 -0300

    add new route to browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard'; 
+import Dashboard from './components/MainDashboard/Dashboard'; 
 import Header from './components/Header';  
 import Footer from './components/Footer';  
-import Home from './components/Home';  
+import Home from './components/Home/Home';  
 import Login from './components/Login';  
 import Signup from './components/Signup';  
 import CompletedTasks from './components/CompletedTasks';
-import Integrations from './components/Integrations';
+import IntegrationsDashboard from './components/IntegrationsDashboard/IntegrationsDashboard';
 
 function App() {
     return (
@@ -20,7 +20,7 @@ function App() {
               <Route path="/login" element={<Login />} /> 
               <Route path="/signup" element={<Signup />} /> 
               <Route path="/completed" element={<CompletedTasks />} />
-              <Route path="/integrations" element={<Integrations />} />
+              <Route path="/integrations" element={<IntegrationsDashboard />} />
             </Routes>
         </main>
         <Footer />
