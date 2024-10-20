63e12cba3bef7e49f8a296e2f6716fc0a38ffbab
sp-jonathan <jonathanshamwana.visa@gmail.com>
2024-10-20 20:31:51 -0300

    add conditional header for logged in user

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import '../styles/Header.css';
+import '../styles/Header.css'
 
 function Header() {
+  const token = sessionStorage.getItem('token');
+
   return (
     <header className="app-header">
       <nav>
         <ul className="nav-links">
           <li><a href="/">Home</a></li>
-          <li><a href="/dashboard">Dashboard</a></li>
-          <li><a href="/completed">Completed</a></li>
-          <li><a href="/integrations">Integrations</a></li>
-          <li><a href="/signup">Signup</a></li>
-          <li><a href="/login">Login</a></li>
+          {token ? (
+            <>
+              <li><a href="/dashboard">Dashboard</a></li>
+              <li><a href="/completed">Completed Tasks</a></li>
+              <li><a href="/integrations">Integrations</a></li>
+              <li><a href="/" onClick={() => {
+                sessionStorage.removeItem('token');
+                window.location.href = '/login';
+              }}>Logout</a></li>
+            </>
+          ) : (
+            <>
+              <li><a href="/login">Login</a></li>
+              <li><a href="/signup">Signup</a></li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
